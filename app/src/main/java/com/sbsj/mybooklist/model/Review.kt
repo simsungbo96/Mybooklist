84b4c8ebe2c57e0d4e071085a9a0c78ff264e1d8
master <simsungbo96@gmail.com>
2022-06-14 10:49:51 +0900

    finish

---
+++ app/src/main/java/com/sbsj/mybooklist/model/Review.kt
@@ -0,0 +1,12 @@
+package com.sbsj.mybooklist.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Review(
+    @PrimaryKey val isbn: String,
+    @ColumnInfo(name = "review") val review: String?
+)
